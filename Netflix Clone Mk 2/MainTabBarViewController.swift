08a75888c34dd45ad3b4059ee04216a2e3074ebf
Noah Pope <npope@tutanota.com>
2023-02-03 08:52:15 -0500

    Initial Commit

---
+++ Netflix Clone Mk 2/MainTabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone Mk 2
+//
+//  Created by Noah Pope on 2/3/23.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+
+}
+
