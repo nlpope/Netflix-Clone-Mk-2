374543c0fe25e180d277b530ca9b79829a77c24d
Noah Pope <npope@tutanota.com>
2023-02-06 08:14:14 -0500

    Adding icons and labels to bottom navigation bar; also set the tint color on the nav bar

--- Netflix Clone Mk 2/Controllers/Core/HomeViewController.swift
+++ Netflix Clone Mk 2/Controllers/Core/HomeViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let homeFeedTable: UITableView = {
+       let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
 
     }
 
