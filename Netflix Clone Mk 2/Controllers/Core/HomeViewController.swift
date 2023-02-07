4caff9a58891df65dbb95c12294c6da89fb2ee6a
Noah Pope <npope@tutanota.com>
2023-02-07 08:05:43 -0500

    Adding delegate and datasource methods to give homeFeedTable something to display

--- Netflix Clone Mk 2/Controllers/Core/HomeViewController.swift
+++ Netflix Clone Mk 2/Controllers/Core/HomeViewController.swift
@@ -4,12 +4,14 @@
 //
 //  Created by Noah Pope on 2/5/23.
 //
+//  first view that shows from boot
 //  this is a cocoa touch class
 
 import UIKit
 
 class HomeViewController: UIViewController {
     
+    //this is a subview
     private let homeFeedTable: UITableView = {
        let table = UITableView()
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -18,9 +20,28 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemBackground
-
+        view.addSubview(homeFeedTable) //subview added, viewDidLayoutSubviews() needed
+        
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
     }
 
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
