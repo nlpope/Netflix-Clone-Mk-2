972fcbe8eb0176fec7bfcd8a801e69bc2643d4ba
Noah Pope <npope@tutanota.com>
2023-02-18 08:50:35 -0500

    adding heroImage asset to top of collection views

--- Netflix Clone Mk 2/Views/HeroHeaderUIView.swift
+++ Netflix Clone Mk 2/Views/HeroHeaderUIView.swift
@@ -14,6 +14,7 @@ class HeroHeaderUIView: UIView {
         let imageView = UIImageView()
         imageView.contentMode = .scaleToFill
         imageView.clipsToBounds = true
+        imageView.image = UIImage(named: "heroImage")
         return imageView
 
     }()
