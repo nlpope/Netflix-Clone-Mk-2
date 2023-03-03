80e09b7b9803beb4a2724ca527830a8b9d9a7a95
Noah Pope <npope@tutanota.com>
2023-03-03 12:57:55 -0500

    creating new "Managers" folder for the APICaller class; inputting API key from TMDB website

---
+++ Netflix Clone Mk 2/Managers/APICaller.swift
@@ -0,0 +1,16 @@
+//
+//  APICaller.swift
+//  Netflix Clone Mk 2
+//
+//  Created by Noah Pope on 3/3/23.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "a33493c4a470b7abe6133ac90dfaed66"
+}
+
+class APICaller {
+    static let shared = APICaller() //shared instance made so we can call all the below methods later
+}
