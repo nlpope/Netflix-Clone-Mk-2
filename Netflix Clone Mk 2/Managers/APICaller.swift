4a0a8331ecc20a91aa932af2c50f857cd9dcfd5c
Noah Pope <npope@tutanota.com>
2023-03-12 16:42:05 -0400

    adding API_KEY and baseURL to use in APICaller > getTrendingMovies()

--- Netflix Clone Mk 2/Managers/APICaller.swift
+++ Netflix Clone Mk 2/Managers/APICaller.swift
@@ -9,13 +9,14 @@ import Foundation
 
 struct Constants {
     static let API_KEY = "a33493c4a470b7abe6133ac90dfaed66"
+    static let baseURL = "https://api.themoviedb.org"
 }
 
 class APICaller {
     static let shared = APICaller() //shared instance made so we can call all the below methods later
     
     func getTrendingMovies(completion: @escaping (String) -> Void) {
-        
+        guard let url = URL(string: "\(Constants.baseURL)/3/movie/550?api_key=\(Constants.API_KEY)") else {return}
     }
     
 }
