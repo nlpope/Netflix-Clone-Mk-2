79df0e35cadeee515e3bc7ab681342d21abe5f6f
Noah Pope <npope@tutanota.com>
2023-03-17 08:25:09 -0400

    creating a structure for resulting data from URL session to be dumped in and used

---
+++ Netflix Clone Mk 2/Models/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  Netflix Clone Mk 2
+//
+//  Created by Noah Pope on 3/17/23.
+//
+
+import Foundation
+
+struct TrendingMoviesResponse {
+    //creating a structure for resulting data from URL session to be dumped in and used
+    let results: [Movie]
+}
+
+struct Movie {
+    
+}
