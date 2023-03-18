6861858bdd89e48bc462974155854f0ce7bc519f
Noah Pope <npope@tutanota.com>
2023-03-18 16:50:06 -0400

    changing Movie struct name to title & adding Title properties

---
+++ Netflix Clone Mk 2/Models/Title.swift
@@ -0,0 +1,25 @@
+//
+//  Movie.swift
+//  Netflix Clone Mk 2
+//
+//  Created by Noah Pope on 3/17/23.
+//
+
+import Foundation
+
+struct TrendingMoviesResponse {
+    //creating a structure for resulting data from URL session to be dumped in and used
+    let results: [Title]
+}
+
+struct Title {
+    let id: Int
+    let media_type: String?
+    let original_name: String?
+    let original_title: String?
+    let poster_path: String?
+    let overview: String?
+    let vote_count: Int
+    let release_date: String?
+    let vote_average: Double
+}
