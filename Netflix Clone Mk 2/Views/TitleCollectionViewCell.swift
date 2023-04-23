a34c8a468e34322b3a55f94c64c5b9ad8b0a6b6a
Noah Pope <npope@tutanota.com>
2023-04-23 15:01:07 -0400

    setting up TitleCollectionViewCell file to have somewhere to plop the data we got from our URLRequest

---
+++ Netflix Clone Mk 2/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix Clone Mk 2
+//
+//  Created by Noah Pope on 4/23/23.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "TitleCollectionViewCell"
+    
+}
