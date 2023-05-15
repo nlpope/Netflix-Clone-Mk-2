e216d44f03361b04dec7d5ca2aebc79797d063e0
Noah Pope <npope@tutanota.com>
2023-05-15 07:01:07 -0400

    setting up UpcomingViewController

--- Netflix Clone Mk 2/Controllers/Core/SearchViewController.swift
+++ Netflix Clone Mk 2/Controllers/Core/SearchViewController.swift
@@ -5,14 +5,16 @@
 //  Created by Noah Pope on 2/5/23.
 //
 
+
+//create a subview identical to UpcomingViewcontroller's
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController {    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemBackground
+        
     }
 
 }
